e3aebc491293f158090f0720f9890c7abb3d3713
muskansaw09 <sawmuskan832@gmail.com>
2025-07-24 16:19:13 +0530

    Add files via upload

---
+++ practical 3..py
@@ -0,0 +1,10 @@
+a =int(input("enter first number:"))
+b=int(input("enter second number:"))
+c=int(input("enter third number:"))
+if a>b and b>c :
+    print("first number is greater")
+elif a<b and b>c:
+    print("second number is greater")
+else:
+    print("third number is greater")
+    
