48a7c80ef8b10c3c906c3be81474c981c041c2dc
muskansaw09 <sawmuskan832@gmail.com>
2025-07-24 16:47:08 +0530

    Add files via upload

---
+++ practical5..............fycs.py
@@ -0,0 +1,12 @@
+num =int(input("Enter a number:"))
+if num <= 1:
+    print("Neither Prime Nor Composite")
+elif num ==2:
+    print("Prime Number")
+else:
+    for i in range(2, num):
+        if num% i ==0:
+            print("Composite Number")
+            break
+    else:
+        print("Prime Number")
